b2ad4ddb76f6af14ecabeb765bec1c27fa57c2bd
allan-mlpe <allan_mlpe@hotmail.com>
2018-02-12 09:04:07 -0300

    Inclusão da sidebar da aplicação

---
+++ front/src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+declare var jquery:any;
+declare var $ :any;
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    $(".button-collapse").sideNav();
+  }
+
+}
