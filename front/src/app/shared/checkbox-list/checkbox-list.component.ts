3248b5c659bb3a10e70bcedc322cfc4822be3cb4
allan-mlpe <allan_mlpe@hotmail.com>
2018-02-24 14:31:52 -0300

    Adaptação na lista de checkbox para emitir lista de objetos selecionados

--- front/src/app/shared/checkbox-list/checkbox-list.component.ts
+++ front/src/app/shared/checkbox-list/checkbox-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-checkbox-list',
@@ -7,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckboxListComponent implements OnInit {
 
+  @Output() 
+  selected: EventEmitter<any> = new EventEmitter<any>();
+  //@Input()
+  selectedList: Array<any> = [
+    { id: 1, name: "Characteristic 1"}
+  ];
+
   propertyList: Array<any> = [
-    { name: "Characteristic 1"},
-    { name: "Characteristic 2"},
-    { name: "Characteristic 3"}
+    { id: 1, name: "Characteristic 1"},
+    { id: 2, name: "Characteristic 2"},
+    { id: 3, name: "Characteristic 3"}
   ];
 
   constructor() { }
@@ -18,4 +25,22 @@ export class CheckboxListComponent implements OnInit {
   ngOnInit() {
   }
 
+  emitSelecteds() {
+    this.selected.emit(this.selectedList);
+  }
+
+  updateSelectedList(property, checkBox: HTMLInputElement) {
+    if(checkBox.
+      checked && !this.isSelected(property)) {
+      // add property to selected list
+      this.selectedList.push(property);
+    } else if(!checkBox.checked && this.isSelected(property)) {
+      // remove property from selected list
+      this.selectedList = this.selectedList.filter(p => p['id'] !== property['id']);
+    }
+  }
+
+  isSelected(property): boolean {
+    return this.selectedList.some(p => p['id'] === property['id']);
+  }
 }
