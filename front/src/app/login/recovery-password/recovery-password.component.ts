da8c0492d0179b625129b6a7aec17a9af3d6efc9
allan-mlpe <allan_mlpe@hotmail.com>
2018-03-15 20:54:06 -0300

    [front#31] Criação do componente de recuperação de senha
    Closes #31

--- front/src/app/login/recovery-password/recovery-password.component.ts
+++ front/src/app/login/recovery-password/recovery-password.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ToastFactory} from "../../shared/toast-factory";
+import {FormValidateUtils} from "../../shared/form-validate-utils";
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-recovery-password',
@@ -7,9 +12,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecoveryPasswordComponent implements OnInit {
 
-  constructor() { }
+  formValidateUtils: FormValidateUtils;
+
+  form: FormGroup;
+
+  constructor(
+    private authService: AuthService,
+    private formBuilder: FormBuilder,
+    private router: Router) { }
 
   ngOnInit() {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
+
+    this.formValidateUtils = new FormValidateUtils(this.form);
+  }
+
+  onSubmit() {
+    if(this.form.valid) {
+      const email = this.form.get('email');
+
+    } else {
+      this.formValidateUtils.checkAllFields(this.form);
+    }
+  }
+
+  showError(field: string): boolean {
+    return this.formValidateUtils.checkInvalidAndTouchedField(field);
+  }
+
+  buildErrorMessage(field: string): string {
+    return this.formValidateUtils.buildErrorMessage(field);
+  }
+
+  addClassError(field: string) {
+    let result = this.showError(field);
+    return {
+      invalid: result
+    }
   }
 
 }
--- front/src/app/shared/form-validate-utils.ts
+++ front/src/app/shared/form-validate-utils.ts
@@ -12,14 +12,14 @@ export class FormValidateUtils {
             console.log(campo);
             const controle = formGroup.get(campo);
             controle.markAsDirty();
-    
+
             if(controle instanceof FormGroup) {
               this.checkAllFields(controle);
             }
           }
         );
     }
-    
+
     checkInvalidAndTouchedField(field: string): boolean {
         return this.form.get(field).invalid && (this.form.get(field).touched || this.form.get(field).dirty);
     }
@@ -27,12 +27,14 @@ export class FormValidateUtils {
     buildErrorMessage(field: string): string {
         let messages: Array<string> = [];
         let errors = this.form.get(field).errors;
-        
+
         if(errors !== null) {
             if(errors.required) {
                 messages.push('this field is required');
             } else if(errors.minlength) {
                 messages.push('this field must be at least 6 characters');
+            } else if(errors.email) {
+                messages.push('insert a valid email')
             }
         }
 
