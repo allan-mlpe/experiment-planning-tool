d3051950b0920790d20ba2642353d9ca4c80116d
allan-mlpe <allan_mlpe@hotmail.com>
2018-03-17 10:31:24 -0300

    Inclusão de interface a ser implementada por conversores para objetos VO

---
+++ back/pcvt/src/main/java/br/ufpe/cin/pcvt/api/converters/IVOConverter.java
@@ -0,0 +1,8 @@
+package br.ufpe.cin.pcvt.api.converters;
+
+/**
+ * @author Allan Monteiro de Lima (aml3@cin.ufpe.br)
+ */
+public interface IVOConverter<A, B> {
+    public B convertToVO(A object);
+}
