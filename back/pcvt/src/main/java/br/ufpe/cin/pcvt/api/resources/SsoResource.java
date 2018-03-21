d4c58652ecf646b4cf3d4fa7ca47f614244c53b3
allan-mlpe <allan_mlpe@hotmail.com>
2018-03-20 22:36:27 -0300

    [back#35] Implementação de atualização de senha
    Closes #35

--- back/pcvt/src/main/java/br/ufpe/cin/pcvt/api/resources/SsoResource.java
+++ back/pcvt/src/main/java/br/ufpe/cin/pcvt/api/resources/SsoResource.java
@@ -2,15 +2,18 @@ package br.ufpe.cin.pcvt.api.resources;
 
 import br.ufpe.cin.pcvt.api.converters.UserVOConverter;
 import br.ufpe.cin.pcvt.api.exceptions.ApiException;
+import br.ufpe.cin.pcvt.api.models.ApiMessage;
 import br.ufpe.cin.pcvt.api.models.Credentials;
 import br.ufpe.cin.pcvt.api.models.UserVO;
 import br.ufpe.cin.pcvt.controllers.ControllerFactory;
 import br.ufpe.cin.pcvt.controllers.UserController;
 import br.ufpe.cin.pcvt.controllers.UserTokenController;
 import br.ufpe.cin.pcvt.data.models.user.User;
+import br.ufpe.cin.pcvt.data.models.user.UserToken;
 import br.ufpe.cin.pcvt.exceptions.InvalidCredentialsException;
 import br.ufpe.cin.pcvt.exceptions.UserDeactivatedException;
 import br.ufpe.cin.pcvt.exceptions.UserNotFoundException;
+import br.ufpe.cin.pcvt.exceptions.UserTokenNotFoundException;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
@@ -77,6 +80,33 @@ public class SsoResource {
             throw  new ApiException(Response.Status.NOT_FOUND,
                     "User not found");
 
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ApiException(Response.Status.INTERNAL_SERVER_ERROR,
+                    "Internal server error");
+        }
+    }
+
+    @POST
+    @Path("resetPassword")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response resetPassword(@FormParam("token") String token,
+                                  @FormParam("password") String password) throws ApiException {
+
+        try {
+            UserToken userToken = userTokenController.getByToken(token);
+            userToken.getUser().setPassword(password);
+            userTokenController.consumeUserToken(userToken);
+
+            ApiMessage msg = new ApiMessage(Response.Status.OK,
+                    "Successful password reset");
+
+            return Response.ok(msg).build();
+
+        } catch (UserTokenNotFoundException e) {
+            throw  new ApiException(Response.Status.BAD_REQUEST,
+                    "Invalid token");
         } catch (Exception e) {
             e.printStackTrace();
             throw new ApiException(Response.Status.INTERNAL_SERVER_ERROR,
