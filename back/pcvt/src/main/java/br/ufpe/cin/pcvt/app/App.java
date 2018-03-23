50e8207c2357e943a469b46ecca58b7320f5ad98
allan-mlpe <allan_mlpe@hotmail.com>
2018-03-23 20:52:19 -0300

    [back#39] Inclusão de filtro para tratar CORS no servidor
    Closes #39

--- back/pcvt/src/main/java/br/ufpe/cin/pcvt/app/App.java
+++ back/pcvt/src/main/java/br/ufpe/cin/pcvt/app/App.java
@@ -1,15 +1,33 @@
 package br.ufpe.cin.pcvt.app;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.FilterHolder;
+import org.eclipse.jetty.servlet.FilterMapping;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.glassfish.jersey.servlet.ServletContainer;
 
 public class App {
 
     public static void main(String[] args) {
+        // CORS filter
+        FilterHolder holder = new FilterHolder(CrossOriginFilter.class);
+        holder.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+        holder.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, "*");
+        holder.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "GET,POST,PUT,HEAD");
+        holder.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "X-Requested-With,Content-Type,Accept,Origin");
+        holder.setInitParameter(CrossOriginFilter.ALLOW_CREDENTIALS_PARAM, "true");
+        holder.setName("cross-origin");
+        FilterMapping fm = new FilterMapping();
+        fm.setFilterName("cross-origin");
+        fm.setPathSpec("*");
+
+
+
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
         context.setContextPath("/");
+        context.getServletHandler().addFilter(holder, fm);
 
         Server jettyServer = new Server(7007);
         jettyServer.setHandler(context);
