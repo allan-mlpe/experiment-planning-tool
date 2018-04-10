a785a4b38a2f4621e5de6223cb6e910334106bd6
allan-mlpe <allan_mlpe@hotmail.com>
2018-04-09 22:35:16 -0300

    Inclusão de classe utilitária contendo as constantes da API

---
+++ back/pcvt/src/main/java/br/ufpe/cin/pcvt/api/resources/APIConstants.java
@@ -0,0 +1,10 @@
+package br.ufpe.cin.pcvt.api.resources;
+
+import javax.ws.rs.core.MediaType;
+
+public class APIConstants {
+
+    private static final String CHARSET_UTF8 = ";charset=utf-8";
+    public static final String APPLICATION_JSON = MediaType.APPLICATION_JSON + CHARSET_UTF8;
+
+}
