593dc3f73893659efb06595f744099e5acb4abad
allan-mlpe <allan_mlpe@hotmail.com>
2018-04-12 20:27:36 -0300

    [front#42] Criação de componente de perfil de usuário

--- front/src/app/profile/profile.component.ts
+++ front/src/app/profile/profile.component.ts
@@ -1,15 +1,89 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormValidateUtils} from "../shared/form-validate-utils";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {IFormCanDeactivate} from "../guards/Iform-candeactivate";
+import {ModalService} from "../services/modal.service";
+import {Subscription} from "rxjs/Rx";
+import {User} from "../model/user";
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, OnDestroy, IFormCanDeactivate {
 
-  constructor() { }
+  loading: boolean = false;
+  formValidateUtils: FormValidateUtils;
+  hasUnsavedChanges: boolean = false;
+
+  form: FormGroup;
+
+  /**
+   * Subscription to listen changes in form
+   */
+  private subsc: Subscription;
+
+  user: User = new User();
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private modalService: ModalService
+  ) { }
 
   ngOnInit() {
+    this.form = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      institution: [''],
+      email: ['', [Validators.required, Validators.email]],
+      profileLink: [''],
+      workArea: [''],
+      isCollaborator: [false],
+      isAvailable: [false]
+    });
+
+    this.formValidateUtils = new FormValidateUtils(this.form);
+    this.subsc = this.form.valueChanges.subscribe(
+      (data) => {
+        this.hasUnsavedChanges = true;
+
+        // if a change already occurred, it's not necessary to keep the subscribe
+        this.subsc.unsubscribe();
+      }
+    );
   }
 
+  ngOnDestroy() {
+    this.subsc.unsubscribe();
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+    console.log(this.user);
+  }
+
+  canDeactivateForm() {
+    return !this.hasUnsavedChanges || this.modalService.showUnsaveChangesModal();
+  }
+
+  showError(field: string): boolean {
+    return this.formValidateUtils.checkInvalidAndTouchedField(field);
+  }
+
+  buildErrorMessage(field: string): string {
+    return this.formValidateUtils.buildErrorMessage(field);
+  }
+
+  addClassError(field: string) {
+    let result = this.showError(field);
+    return {
+      invalid: result
+    }
+  }
+
+  autoFill(field: string): any {
+    return {
+      active: this.user[field] !== undefined
+    }
+  }
 }
