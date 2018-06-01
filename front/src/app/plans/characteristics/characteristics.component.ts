2de2e08d983b76dea206379a0a7a74a84eea5f13
allan-mlpe <allan_mlpe@hotmail.com>
2018-05-31 22:42:21 -0300

    Inclusão de persistência de plano experimental em localStorage

--- front/src/app/plans/characteristics/characteristics.component.ts
+++ front/src/app/plans/characteristics/characteristics.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SIMPLE_OPTIONS } from '../../model/simple-options'
+import {PcvtConstants} from "../../shared/pcvt-constants";
+import {ActivatedRoute, Router} from "@angular/router";
+import {ToastFactory} from "../../shared/toast-factory";
 
 @Component({
   selector: 'app-characteristics',
@@ -9,16 +12,41 @@ import { SIMPLE_OPTIONS } from '../../model/simple-options'
 export class CharacteristicsComponent implements OnInit {
 
   options = SIMPLE_OPTIONS;
-  
-  characteristics: Array<any> = [
-    { id: 1, text: "Characteristic 1"},
-    { id: 2, text: "Characteristic 2"},
-    { id: 3, text: "Characteristic 3"}
-  ];
+  characteristics: Array<any> = [];
+  private planId: string;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.buildCharacteristicsObject();
+    this.planId = this.route.snapshot.params['id'];
+
+    const planCharacteristics = localStorage.getItem(this.planId);
+
+    if(planCharacteristics !== null) {
+      this.characteristics = JSON.parse(planCharacteristics);
+    } else {
+      this.buildCharacteristicsObject();
+    }
   }
 
+  buildCharacteristicsObject() {
+    const questions: Array<any> = PcvtConstants.CHARACTERIZATION_QUESTIONS;
+
+    questions.forEach(category => {
+      category.questions.forEach(question => {
+        this.characteristics.push(question);
+      })
+    });
+  }
+
+  saveCharacteristics(event) {
+    localStorage.setItem(this.planId, JSON.stringify(this.characteristics));
+
+    ToastFactory.successToast('Characteristics of the plan defined')
+    this.router.navigate(['../workspace'], {relativeTo: this.route });
+  }
 }
