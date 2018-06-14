07dd8ab981d892cbe489a9af24b59159aaeddd9d
allan-mlpe <allan_mlpe@hotmail.com>
2018-06-14 10:18:09 -0300

    [front#73] Componente para criação de novas revisões
    Closes #73

--- front/src/app/reviews/create-review/create-review.component.ts
+++ front/src/app/reviews/create-review/create-review.component.ts
@@ -2,7 +2,6 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {IFormCanDeactivate} from "../../guards/Iform-candeactivate";
 import {Subscription} from "rxjs/Rx";
 import {ModalService} from "../../services/modal.service";
-import {PlanService} from "../../services/plan.service";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {ActivatedRoute, Router} from "@angular/router";
 import {FormValidateUtils} from "../../shared/form-validate-utils";
@@ -11,6 +10,7 @@ import {User} from "../../model/user";
 import {Plan} from "../../model/plan";
 import {ApiMessage} from "../../model/pcvt-message";
 import {ToastFactory} from "../../shared/toast-factory";
+import {ReviewsService} from "../../services/reviews.service";
 
 @Component({
   selector: 'app-create-review',
@@ -44,10 +44,13 @@ export class CreateReviewComponent implements OnInit, OnDestroy, IFormCanDeactiv
    */
   private hasUnsavedChanges: boolean = false;
 
+  loading: boolean = true;
+  loadingButton: boolean = false;
+
   constructor(
     private formBuilder: FormBuilder,
-    private planService: PlanService,
     private userService: UserService,
+    private reviewsService: ReviewsService,
     private router: Router,
     private route: ActivatedRoute,
     private modalService: ModalService
@@ -61,7 +64,7 @@ export class CreateReviewComponent implements OnInit, OnDestroy, IFormCanDeactiv
     );
 
     this.form = this.formBuilder.group({
-      reviewers: ['', [Validators.required]],
+      reviewers: [''],
       expireDate: ['', [Validators.required]]
     });
 
@@ -75,7 +78,9 @@ export class CreateReviewComponent implements OnInit, OnDestroy, IFormCanDeactiv
       }
     )
 
-    this.userService.getAvailable().subscribe(
+    this.userService.getAvailable()
+      .finally(() => this.loading = false)
+      .subscribe(
       (data: Array<User>) => {
         this.availableReviewers = data;
 
@@ -93,17 +98,45 @@ export class CreateReviewComponent implements OnInit, OnDestroy, IFormCanDeactiv
     this.todayDate = `${date.getFullYear()}-${date.getMonth() < 10 ? '0'+(date.getMonth()+1) : (date.getMonth()+1)}-${date.getDate() < 10 ? '0'+date.getDate() : date.getDate()}`;
   }
 
-  selectItem(user, index, target) {
-    if(target['checked']) {
-      this.selectedReviewers.push(user);
-    } else {
+  selectItem(user, target) {
+    const index: number = this.selectedReviewers.indexOf(user);
+    if(index !== -1) {
       this.selectedReviewers.splice(index, 1);
+    } else {
+      this.selectedReviewers.push(user);
+    }
+
+    if(this.selectedReviewers.length === 0) {
+      this.form.get('reviewers').setErrors({required: true});
     }
   }
 
   onSubmit() {
     if (this.form.valid) {
-      ToastFactory.infoToast('Available soon');
+      this.loadingButton = true;
+      console.log(this.selectedReviewers);
+      console.log(this.form.get('expireDate').value);
+      const dateField = new Date(Date.parse(this.form.get('expireDate').value)) ;
+
+      const reviewWrapperObj: any = {
+        plan: this.plan,
+        reviewers: this.selectedReviewers,
+        date: dateField
+      }
+
+      this.reviewsService.createReview(reviewWrapperObj)
+        .finally(() => this.loadingButton = false)
+        .subscribe(
+        (data: ApiMessage) => {
+          ToastFactory.successToast(data.message);
+          this.hasUnsavedChanges = false;
+          this.navigateToReadyToReviewList();
+        },
+        (err: ApiMessage) => {
+          console.log(err);
+          ToastFactory.errorToast(err.message);
+        }
+      )
 
     } else {
       this.formValidateUtils.checkAllFields(this.form);
@@ -129,7 +162,7 @@ export class CreateReviewComponent implements OnInit, OnDestroy, IFormCanDeactiv
     return !this.hasUnsavedChanges || this.modalService.showUnsaveChangesModal();
   }
 
-  cancel() {
+  navigateToReadyToReviewList() {
     this.router.navigate(['../../'], {relativeTo: this.route})
   }
 
