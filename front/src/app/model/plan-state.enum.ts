4eae52d7a049e80484e37168e3b2daef5186a0a9
allan-mlpe <allan_mlpe@hotmail.com>
2018-06-16 22:26:07 -0300

    Inclusão de enums para status de planos e revisões

---
+++ front/src/app/model/plan-state.enum.ts
@@ -0,0 +1,11 @@
+export enum PlanState {
+  Planning = "Planning",
+  ReadyToReview = "Ready to review",
+  WaitingReview = "Waiting review",
+  Refused = "Refused",
+  Reviewing = "Reviewing",
+  Canceled = "Canceled",
+  Expired = "Expired",
+  PartiallyCompleted = "Partially completed",
+  Completed = "Completed"
+}
---
+++ front/src/app/model/review-state.enum.ts
@@ -0,0 +1,9 @@
+export enum ReviewState {
+  ReviewRequested = "Review requested",
+  Reviewing = "Reviewing",
+  Refused = "Refused",
+  Canceled = "Canceled",
+  Expired = "Expired",
+  Completed = "Completed",
+  Invalid = "Invalid"
+}
