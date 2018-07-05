90dd04351479177746d738772f2081a487bd6537
allan-mlpe <allan_mlpe@hotmail.com>
2018-07-05 19:59:30 -0300

    Mudança de label para estados do plano experimental

--- front/src/app/model/plan-state.enum.ts
+++ front/src/app/model/plan-state.enum.ts
@@ -2,10 +2,10 @@ export enum PlanState {
   Planning = "Planning",
   ReadyToReview = "Ready to review",
   WaitingReview = "Waiting review",
-  Refused = "Refused",
+  Refused = "Refused review",
   Reviewing = "Reviewing",
   Canceled = "Canceled",
-  Expired = "Expired",
+  Expired = "Time expired",
   PartiallyCompleted = "Partially completed",
   Completed = "Completed"
 }
