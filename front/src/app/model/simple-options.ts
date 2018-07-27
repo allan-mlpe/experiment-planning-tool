7b7af54f03c027faeec943f03a5be39f6f4bc485
allan-mlpe <allan_mlpe@hotmail.com>
2018-07-27 12:59:39 -0300

    Ajuste em texto explicativo do item "Partially"

--- front/src/app/model/simple-options.ts
+++ front/src/app/model/simple-options.ts
@@ -1,6 +1,6 @@
 export const SIMPLE_OPTIONS = [
     {id: 1, text: 'Yes', value: 'YES', hint: 'Means that your experiment has this characteristic'},
-    {id: 2, text: 'Partially', value: 'PARTIALLY', hint: 'Means that your experiment has this characteristic partially'},
+    {id: 2, text: 'Partially', value: 'PARTIALLY', hint: 'Means that your experiment may have this characteristic, but you are not sure.'},
     {id: 3, text: 'No', value: 'NO', hint: 'Means that your experiment has not this characteristic'},
     {id: 4, text: 'Not applicable', value: 'NOT_APPLICABLE', hint: 'Means that the item is not applicable for your experiment'}
 ];
